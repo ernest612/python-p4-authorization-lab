aebfcb1696d2cfc25ee38f86a8604b45d3b3bbcb
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-10-23 22:34:04 +0300

    PUBLISH

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify, request, session
@@ -87,12 +88,20 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized access'}, 401
+        articles = [article.to_dict() for article in Article.query.filter_by(is_member_only=True).all()]
+        return make_response(jsonify(articles), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized access'}, 401
+        article = Article.query.get(id)
+        if article and article.is_member_only:
+            return article.to_dict(), 200
+        return {'message': 'Article not found or not a member-only article'}, 404
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
